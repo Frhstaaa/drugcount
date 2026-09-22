d3db1193a4cdf3b2e05d9bd552fe00ae43982413
Frhstaaa <tiyasfrahesta888@gmail.com>
2026-09-22 20:55:31 +0700

    Add public web wrappers for deploy.php and update.php

---
+++ public/deploy.php
@@ -0,0 +1,3 @@
+<?php
+// Wrapper to run deploy.php from public web root
+require_once dirname(__DIR__) . '/deploy.php';
---
+++ public/update.php
@@ -0,0 +1,3 @@
+<?php
+// Wrapper to run update.php from public web root
+require_once dirname(__DIR__) . '/update.php';
